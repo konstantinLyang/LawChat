7f2e5b40d1ee7697978093fecf352824bf941910
Konstantin Lyang <konst613@mail.ru>
2023-03-19 17:24:07 +0700

    dev: login window design

---
+++ lawChat.Client/ViewModel/LoginWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using lawChat.Client.ViewModel.Base;
+
+namespace lawChat.Client.ViewModel
+{
+    internal class LoginWindowViewModel : ViewModelBase
+    {
+        private string _loginTextBox;
+        public string LoginTextBox
+        {
+            get => _loginTextBox;
+            set => Set(ref _loginTextBox, value);
+        }
+
+        private string _passwordTextBox;
+        public string PasswordTextBox
+        {
+            get => _passwordTextBox;
+            set => Set(ref _passwordTextBox, value);
+        }
+    }
+}
