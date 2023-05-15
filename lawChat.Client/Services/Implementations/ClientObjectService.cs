080cdf3b1b6792da5fd5a21094bafd9d84da3c39
Konstantin Lyang <konst613@mail.ru>
2023-05-15 08:53:10 +0700

    ip

--- lawChat.Client/Services/Implementations/ClientObjectService.cs
+++ lawChat.Client/Services/Implementations/ClientObjectService.cs
@@ -21,7 +21,7 @@ namespace lawChat.Client.Services.Implementations
             {
                 _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-                IPEndPoint serverEndPoint = new(IPAddress.Parse("10.10.11.47"), 8080);
+                IPEndPoint serverEndPoint = new(IPAddress.Parse("127.0.0.1"), 8080);
 
                 _clientSocket.Connect(serverEndPoint);
 
